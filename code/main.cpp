a367bd4dc42973b6cdb72242aac16f422bc2f2bd
dahstar <davood.abbaspour@gmail.com>
2017-01-18 17:42:58 +0330

    just test netbeans

---
+++ code/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: davood
+ *
+ * Created on January 18, 2017, 4:57 PM
+ */
+
+#include <iostream>
+#include "union.h"
+using namespace std;
+
+/*
+ * 
+ * 
+ */
+int main(int argc, char** argv) {
+     test(5);
+    return 0;
+}
+
